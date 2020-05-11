a2c8120165c3ffe68bd49c8b09f005ec208a4006
TRIVIAHUB72 <64947980+TRIVIAHUB72@users.noreply.github.com>
2020-05-11 09:44:19 +0530

    Update scan.js

--- src/reader/scan.js
+++ src/reader/scan.js
@@ -15,7 +15,7 @@ const storeQuestion = (question) => {
 export default function scan(socketUrl) {
   const ws = new WebSocket(socketUrl, {
     headers: {
-      Authorization: `Bearer ${process.env.BEARER_TOKEN}`,
+      Authorization: `Bearer ${process.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjI2Njc5NjI5LCJ1c2VybmFtZSI6IkFMRVg0NjYzIiwiYXZhdGFyVXJsIjoiZ3M6Ly9jZG4tcHJvZC1oeXBlLXNwYWNlL2RhL2dyZWVuLnBuZyIsInRva2VuIjpudWxsLCJyb2xlcyI6W10sImNsaWVudCI6IiIsImd1ZXN0SWQiOm51bGwsInYiOjEsImlhdCI6MTU4OTA4NDEzMiwiZXhwIjoxNTk2ODYwMTMyLCJpc3MiOiJoeXBlcXVpei8xIn0.R64q87O8LxW1m-vnvo0TWSJl2netNDMVAzD87RjwjB0}`,
       'User-Agent': 'okhttp/3.8.0',
       'x-hq-client': 'Android/1.5.1',
       'x-hq-lang': 'en',
